be10d589b3d8efe51c051ed6ec392cfed28d7979
Mohamed Mustafa M Sarhan <76625670+mohamedmustafa455@users.noreply.github.com>
2025-09-10 20:59:26 +0300

    Update careers-page-client.tsx

--- components/careers-page-client.tsx
+++ components/careers-page-client.tsx
@@ -64,7 +64,7 @@ export default function CareersPageClient() {
     setIsSubmitting(true)
     
     try {
-      const response = await fetch("https://formspree.io/f/mkgzynnp", {
+      const response = await fetch("https://formspree.io/f/xkgvykza", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
